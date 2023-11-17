565c9bef65c09d3b8dbf21bc3c9f0e7ddbcec970
Conghan Huang <53137658+jazzwhite@users.noreply.github.com>
2023-11-17 19:01:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '可可爱爱宝儿',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oofmj6lweLz3Rg9811XZPC8--28o',
+      id: 'oofmj6tLUqO30Ujaqf0LGLySiJmE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'CsqqMqvTvz1WPIBMvA_UoN2xc-f8rzIgt643jTSmrb0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
