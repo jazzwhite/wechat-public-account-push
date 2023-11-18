e146f60e2426a22c1a02e3754ace55dc9b95bafa
Conghan Huang <53137658+jazzwhite@users.noreply.github.com>
2023-11-18 11:01:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oofmj6tLUqO30Ujaqf0LGLySiJmE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'iuUigf-F7FIA1GUtijo49rjw3yf7nUZWHcidvnr5sHU',
+      useTemplateId: 'E17gnK9lP5iBpZ2uHmEHUmDCDkKZ_cOT9zPpraIue88',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-08',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'oofmj6qlhtwgHu63g7svuB8ZNvbQ',
+  CALLBACK_TEMPLATE_ID: 'E17gnK9lP5iBpZ2uHmEHUmDCDkKZ_cOT9zPpraIue88',
 
   CALLBACK_USERS: [
     {
